1882aa884882ca55070a0e1765b55e5b0309cf14
Russell Gordon <rgordon@lcs.on.ca>
2024-11-17 13:31:29 -0500

    Initial Commit

---
+++ ShapeProtocolPlanner/ShapeProtocolPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeProtocolPlannerApp.swift
+//  ShapeProtocolPlanner
+//
+//  Created by Russell Gordon on 2024-11-17.
+//
+
+import SwiftUI
+
+@main
+struct ShapeProtocolPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
